5fae2135862296c924cd75184c6a7152b9aeace3
jboyoluwa3d <justiceemeana@gmail.com>
2023-06-19 09:48:40 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,14 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual puzzle."
+ * main - prints "Programming is like building a multilingual puzzle
  * return 0.
  */
 
 int main(void)
 
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle);
 
 	return(0);
 }
